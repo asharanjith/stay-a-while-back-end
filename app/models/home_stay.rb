f587de6705cb9823730766b5d5f6fcbffd0523fe
admirerbrown <admirerbrown@gmail.com>
2023-05-04 09:33:04 +0000

    nothing new

--- app/models/home_stay.rb
+++ app/models/home_stay.rb
@@ -8,6 +8,6 @@ class HomeStay < ApplicationRecord
   validates :description, presence: true, length: { maximum: 1000 }
   validates :no_of_rooms, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :ratings, presence: true
+  validates :rating, presence: true
   validates :user_id, presence: true
 end
