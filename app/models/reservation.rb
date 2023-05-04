783fe6fe5529e3852c0d05f01664e0f05b1d11c0
elarhadu <elarhadu@gmail.com>
2023-05-04 09:13:45 +0000

    Create reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :home_stay
+
+  validates :no_of_persons, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
