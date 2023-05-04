fadab9501a90a5696c99c2647c48892fecee38a6
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-04 15:51:34 +0530

    add relationship for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :home_stays
+  has_many :reservations
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { minimum: 3 }
