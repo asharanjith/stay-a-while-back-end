dbf64bca0a785825305400559f353e7c918cfc02
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-04 16:16:29 +0530

    update create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
